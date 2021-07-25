cbc5b73f8b7c7a25bb0d49fc5ef6cd865f57e15a
lupeng@iMac <pengloo53@gmail.com>
2021-07-25 21:15:05 +0800

    v2.0 beta

--- server/app.py
+++ server/app.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 import os
 import json
 import time
-from server.kindle.parser import parse_csv_file, parse_html_file
-from poster import post_all_to_flomo, format_data_to_json
+from kindle.parser import parse_csv_file, parse_html_file
+from poster import post_to_flomo, format_data_to_content
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['csv', 'html'])
@@ -39,49 +39,26 @@ def parse():
     return jsonify(result)
 
 
-# 批量导入flomo服务
+# 导入 MEMO (单条)
 @app.route('/post', methods=['POST'])
 def post():
     if request.method == 'POST':
-        print(request.form)
         api = request.form.get('api')
-        tag = request.form.get('tag')
-        delimiter = request.form.get('delimiter')
-        data = request.form.get('data') or []
-        data_list = json.loads(data)
-        # flomo api 有 100 条每天的限制
-        if len(data_list) > 100:
-            return {
-                "code": 1,
-                "message": "Limit on the number of requests per day"
-            }
-        data_json = format_data_to_json(data_list, tag, delimiter)
-        is_order = request.form.get('is_order') or 'false'
-        if is_order == 'false':
-            is_order = False
+        tag = request.form.get('tag') or ''
+        delimiter = request.form.get('delimiter') or ''
+        highlight = request.form.get('highlight') or ''
+        note = request.form.get('note') or ''
+        order = request.form.get('order')
+        content = format_data_to_content({
+            'tag': tag,
+            'delimiter': delimiter,
+            'note': note,
+            'highlight': highlight
+        }, order)
+        if api:
+            return post_to_flomo(content, api)
         else:
-            is_order = True
-        if api and len(data):
-            return post_all_to_flomo(data_json, api, is_order)
-
-# 导入 MEMO (单条)
-@app.route('/postone',method=['POST'])
-def postone():
-    if request.method == 'POST':
-        print(request.form)
-        api = request.form.get('api')
-        tag = request.form.get('tag')
-        delimiter = request.form.get('delimiter')
-        content = request.form.get('content')
-
-        # data_json = format_data_to_json(data_list, tag, delimiter)
-        is_order = request.form.get('is_order') or 'false'
-        if is_order == 'false':
-            is_order = False
-        else:
-            is_order = True
-        if api and len(data):
-            return post_all_to_flomo(data_json, api, is_order)
+            return 'No api'
 
 
 if __name__ == '__main__':
--- server/poster.py
+++ server/poster.py
@@ -9,10 +9,15 @@ import requests
 
 def format_data_to_content(data, order):
     if order == 'up':
-        if "note" in data:
-            content = data["tag"] + '\n' + data["highlight"] + '\n' + data["delimiter"] + '\n<b>' + data["note"] + '</b>'
+        if data['note']:
+            content = data["tag"] + '\n' + data["highlight"] + '\n' + data["delimiter"] + '\n笔记：' + data["note"]
         else:
             content = data["tag"] + '\n' + data["highlight"]
+    elif order == 'down':
+        if data['note']:
+            content = data["highlight"] + '\n' + data["delimiter"] + '\n笔记：' + data["note"] + '\n' + data['tag']
+        else:
+            content = data["highlight"] + '\n' + data['tag']
     return content
 
 
@@ -21,26 +26,8 @@ def post_to_flomo(content, api):
     headers = {
         'Content-Type': 'application/json; charset=UTF-8'
     }
-    return requests.post(api, data=s, headers=headers)
-
-
-# def post_all_to_flomo(json_data, api, is_order):
-#     # 顺序查看，那么需要倒序插入笔记
-#     if is_order:
-#         json_data.reverse()
-#     for index, content in enumerate(json_data):
-#         s = json.dumps({'content': content})
-#         headers = {
-#             'Content-Type': 'application/json; charset=UTF-8'
-#         }
-#         r = requests.post(api, data=s, headers=headers)
-#         # 需要顺序查看，才等待1s，保证页面效果
-#         if is_order:
-#             time.sleep(1)
-#     return {
-#         "code": 0,
-#         "message": "总共 " + str(len(json_data)) + " 条，全部导入完成。"
-#     }
+    r = requests.post(api, data=s, headers=headers)
+    return json.loads(r.text)
 
 
 
